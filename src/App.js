7a49d49e5cfdf4390575184692d3fe2f6d4a406d
Ben Harris <benjaminharris28@gmail.com>
2020-01-30 00:22:09 -0500

    new file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import Context from './Context';
+import dummyStore from './dummyStore';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  // Add state
+  // Add routes
+  // Conditionally render the routes
+  // Set up values for provider component
+  // Wrap app in provider component
+  state = {
+    notes: dummyStore.notes,
+    folders: dummyStore.folders
+  }
+
+  handleAddNote = (note) => {
+    this.setState({
+      notes: [...this.state.notes, note]
+    })
+  }
+
+  handleDeleteNote = (note) => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== note)
+    })
+  }
+
+  handleAddFolder = (folder) => {
+    this.setState({
+      folders: [...this.state.folders, folder]
+    })
+  }
+
+  // Routes
+  
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
 }
 
 export default App;
